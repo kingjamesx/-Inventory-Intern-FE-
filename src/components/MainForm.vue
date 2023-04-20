30030d8e31a885945bd7baf0e7133460a5dd36e9
preye omusuku <omusukup@yahoo.com>
2023-04-20 13:26:52 +0100

    added create inventory page

---
+++ src/components/MainForm.vue
@@ -0,0 +1,23 @@
+<template>
+  <q-form class="column q-pa-md shadow-2">
+    <label>Product Name</label>
+    <LongInput :placeholder="'Canon EOS Rebel T7'" />
+    <SelectInput />
+    <textarea name="body" placeholder="Your text goes here" class="q-mb-md" />
+    <div class="flex q-ml-md">
+      <LongInput :placeholder="'Selling price'" />
+      <LongInput :placeholder="'Cost price'" />
+    </div>
+  </q-form>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+import LongInput from "components/LongInput.vue";
+import SelectInput from "./SelectInput.vue";
+
+export default defineComponent({
+  name: "MainForm",
+  components: { LongInput, SelectInput },
+});
+</script>
---
+++ src/pages/CreateInventory.vue
@@ -0,0 +1,20 @@
+<template>
+  <q-page class="flex-center">
+    <div>
+      <h4 class="q-my-md">Inventory</h4>
+    </div>
+    <q-card>
+      <MainForm />
+    </q-card>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+import MainForm from "src/components/MainForm.vue";
+
+export default defineComponent({
+  name: "CreateInventory",
+  components: { MainForm },
+});
+</script>
