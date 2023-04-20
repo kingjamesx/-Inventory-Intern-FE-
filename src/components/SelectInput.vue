a919081547e0b1dd29140d42dace97392ff040d1
preye omusuku <omusukup@yahoo.com>
2023-04-20 14:26:24 +0100

    added changes

---
+++ src/components/SelectInput.vue
@@ -0,0 +1,25 @@
+<template>
+  <q-select
+    outlined
+    v-model="model"
+    :options="options"
+    label="select product category"
+    class="q-mb-md"
+  />
+</template>
+
+<script>
+import { defineComponent } from "vue";
+import { ref } from "vue";
+
+export default defineComponent({
+  name: "SelectInput",
+
+  setup() {
+    return {
+      model: ref(null),
+      options: ["Google", "Facebook", "Twitter", "Apple", "Oracle"],
+    };
+  },
+});
+</script>
--- src/pages/createInventory.vue
+++ src/pages/createInventory.vue
@@ -1,13 +1,20 @@
 <template>
+  <q-page class="flex-center">
     <div>
-   <h1>Create Inventory</h1>
+      <h4 class="q-my-md">Inventory</h4>
     </div>
+    <q-card>
+      <MainForm />
+    </q-card>
+  </q-page>
 </template>
 
 <script>
+import { defineComponent } from "vue";
+import MainForm from "src/components/MainForm.vue";
 
+export default defineComponent({
+  name: "createInventory",
+  components: { MainForm },
+});
 </script>
-
-<style lang="scss" scoped>
-
-</style>
